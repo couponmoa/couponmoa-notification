16f63c4dbc392e6d42b8c53a8ff7fcdd5127c7f9
plusmate <psejin1478@gmail.com>
2025-04-10 20:44:59 +0900

    fix:AWS 설정 key 수정

---
+++ src/test/java/com/couponmoa/backend/couponmoanotification/CouponmoaNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoanotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponmoaNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
