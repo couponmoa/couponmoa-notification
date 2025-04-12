c52d29182a00ef625098a0772cd9e94e7076e249
나연 <kny5579@naver.com>
2025-04-12 19:09:29 +0900

    refactor(#4): url 주소 변경

--- src/main/java/com/couponmoa/backend/couponmoanotification/controller/SseController.java
+++ src/main/java/com/couponmoa/backend/couponmoanotification/controller/SseController.java
@@ -2,24 +2,28 @@ package com.couponmoa.backend.couponmoanotification.controller;
 
 import com.couponmoa.backend.couponmoanotification.infra.SseEmitterManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/sse")
+@RequestMapping("/api/v1/users")
 public class SseController {
 
     private final SseEmitterManager sseEmitterManager;
 
-    // 클라이언트와 sse 연결
-    @GetMapping("/subscribe/{userId}")
+    // SseEmitter 방식
+    // 클라이언트와 sse 연결 (클라이언트 측에서 실행하는 api)
+    @GetMapping("/{userId}/notifications/emitter")
     public SseEmitter subscribe(@PathVariable Long userId) {
         SseEmitter emitter = new SseEmitter(30 * 60 * 1000L); // 30분 타임아웃
         sseEmitterManager.add(userId, emitter);
         return emitter;
     }
+
+
 }
 
